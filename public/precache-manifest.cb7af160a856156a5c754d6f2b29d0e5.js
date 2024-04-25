58e2bf8f57c520c126f2507797b9bdceda37a003
laharsh <laharshmoturi@gmail.com>
2024-04-24 23:51:23 -0500

    final

---
+++ public/precache-manifest.cb7af160a856156a5c754d6f2b29d0e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c747bf8c37e0b2127cbfa7c8f3ef00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2abe7ead1f0bd23270a2",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "aefd8c93830699527847",
+    "url": "/static/css/main.d73500bb.chunk.css"
+  },
+  {
+    "revision": "2abe7ead1f0bd23270a2",
+    "url": "/static/js/2.b53ab1b3.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.b53ab1b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aefd8c93830699527847",
+    "url": "/static/js/main.a5fe70a1.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
